004c9d297773b3b25abee617ec88a9920dfed169
mayureshtalentica <72433512+mayureshtalentica@users.noreply.github.com>
2020-10-21 18:56:59 +0530

    Update simple_server.py

--- simple_server.py
+++ simple_server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     """Print 'Hello, world!' as the response body."""
-    return 'hello world, Coming form terrform after policy modify v11!!'
+    return 'hello world, Coming form final change!'
 
 @app.route('/3ri')
 def ri_tech():
